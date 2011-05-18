5bbcb0b262224a7c23735c0941faea029ce91fc2
Georg Hofferek <georg.hofferek@gmx.net>
2011-05-18 06:22:09 +0000

    Added some more classes for formula parts. (Still "empty")

---
+++ src/at/iaik/suraq/formula/DomainTermMacro.java
@@ -0,0 +1,12 @@
+/**
+ * Author: Georg Hofferek <georg.hofferek@iaik.tugraz.at>
+ */
+package at.iaik.suraq.formula;
+
+/**
+ * @author Georg Hofferek <georg.hofferek@iaik.tugraz.at>
+ *
+ */
+public class DomainTermMacro extends TermMacro {
+    // TODO implement!
+}
---
+++ src/at/iaik/suraq/formula/TermMacro.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Georg Hofferek <georg.hofferek@iaik.tugraz.at>
+ */
+package at.iaik.suraq.formula;
+
+/**
+ * @author Georg Hofferek <georg.hofferek@iaik.tugraz.at>
+ * 
+ */
+public abstract class TermMacro extends Term {
+
+    /**
+     * The (term) macro.
+     */
+    protected FunctionMacroInstance instance;
+}
