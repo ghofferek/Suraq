836766d16f88430e2d1bd5f0e8f6265773c18786
Georg Hofferek <georg.hofferek@gmx.net>
2014-02-10 16:30:26 +0000

    fixed division by 0

--- src/at/iaik/suraq/proof/VeritProof.java
+++ src/at/iaik/suraq/proof/VeritProof.java
@@ -550,7 +550,8 @@ public class VeritProof implements Serializable {
                 + " splitter threads.");
 
         // Last thread takes the "remainder" of the division
-        final int numNodesPerThread = leavesToClean.size() / (numThreads - 1);
+        final int numNodesPerThread = leavesToClean.size()
+                / ((numThreads - 1) == 0 ? 1 : (numThreads - 1));
         UncolorableLeafSplitter[] splitters = new UncolorableLeafSplitter[numThreads];
         Iterator<VeritProofNode> nodeIterator = leavesToClean.iterator();
         for (int id = 0; id < numThreads; id++) {
