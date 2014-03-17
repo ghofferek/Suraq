3c8b228ae9de1e8e62704c5cad7b10733e9b9cb5
Georg Hofferek <georg.hofferek@gmx.net>
2014-03-17 12:07:46 +0000

    some changes to aiger conversion script

--- lib/simplifier/simplify.sh
+++ lib/simplifier/simplify.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
 ./lib/simplifier/aigtoaig $1 ./binary_aiger.aig
-./lib/simplifier/abc read_aiger ./binary_aiger.aig; strash; refactor; rewrite; dfraig; rewrite; dfraig; write_aiger -s simplified_binary_aiger.aig
-./lib/simplifier/aigtoaig -a simplified_binary_aiger.aig  $2
+./lib/simplifier/abc -c "read_aiger ./binary_aiger.aig; strash; refactor; rewrite; dfraig; rewrite; dfraig; write_aiger -s simplified_binary_aiger.aig"
+./lib/simplifier/aigtoaig -s simplified_binary_aiger.aig  $2
 
