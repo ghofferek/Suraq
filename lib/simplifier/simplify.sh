33ebe4909a81086858ff85a8217678c7cb384dbe
Georg Hofferek <georg.hofferek@gmx.net>
2014-03-17 15:48:53 +0000

    removing intermediate files in abc optimization in order not to hide errors.

--- lib/simplifier/simplify.sh
+++ lib/simplifier/simplify.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ./lib/simplifier/aigtoaig $1 ./binary_aiger.aig
 ./lib/simplifier/abc -c "read_aiger ./binary_aiger.aig; strash; refactor; rewrite; dfraig; rewrite; dfraig; write_aiger -s simplified_binary_aiger.aig"
-./lib/simplifier/aigtoaig -s simplified_binary_aiger.aig  $2
+rm binary_aiger.aig
+./lib/simplifier/aigtoaig simplified_binary_aiger.aig  $2
+rm simplified_binary_aiger.aig
+
 
