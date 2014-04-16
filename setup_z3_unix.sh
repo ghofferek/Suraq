5e829f83468f34255ed3b4deec8e547f2517a818
Georg Hofferek <georg.hofferek@gmx.net>
2014-04-16 16:57:40 +0200

    updated setup scripts to include z3-4.0

--- setup_z3_unix.sh
+++ setup_z3_unix.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd lib
+wget http://research.microsoft.com/projects/z3/z3-4.0.tar.gz
+tar xfz z3-4.0.tar.gz
+rm z3-4.0.tar.gz
+mv z3 z3-4.0
 wget http://research.microsoft.com/projects/z3/z3-3.2.tar.gz
-tar xvfz z3-3.2.tar.gz
+tar xfz z3-3.2.tar.gz
 rm z3-3.2.tar.gz
--- setup_z3_win.cmd
+++ setup_z3_win.cmd
@@ -5,3 +5,8 @@ msiexec /a z3-3.2.msi /qb TARGETDIR=%TEMP%\z3
 move %TEMP%\z3 .
 del z3-3.2.msi
 del z3\z3-3.2.msi
+utilities\wget http://ftp.research.microsoft.com/downloads/0a7db466-c2d7-4c51-8246-07e25900c7e7/z3-4.0.msi
+msiexec /a z3-4.0.msi /qb TARGETDIR=%TEMP%\z3-4.0
+move %TEMP%\z3-4.0 .
+del z3-4.0.msi
+del z3-4.0\z3-4.0.msi
